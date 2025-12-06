6bbfe88f7ed5643ddd9a2e78ff3f440dc3dbbd53
iustinzalog <iustinzalog@gmail.com>
2025-12-06 16:41:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,5 +13,16 @@ function handleResize() {
     checkbox.checked = false;
   }
 }
+const viewport = document.querySelector(".container");
+let isScrolling;
+viewport.addEventListener("scroll", () => {
+  window.clearTimeout(isScrolling);
+  isScrolling = setTimeout(() => {
+    // on scroll end, compute nearest slide and snap
+    const slideWidth = window.innerWidth;
+    const index = Math.round(viewport.scrollLeft / slideWidth);
+    viewport.scrollTo({ left: index * slideWidth, behavior: "smooth" });
+  }, 80); // 80ms after last scroll event
+});
 
 window.addEventListener("resize", handleResize);
